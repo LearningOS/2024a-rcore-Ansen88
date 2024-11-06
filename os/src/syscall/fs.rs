f301b57738c5f5f5dc208b71c69a604977fe8fdc
Ansen <alj79@sohu.com>
2024-11-06 23:41:00 +0800

    update

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -1,6 +1,6 @@
 //! File and filesystem-related syscalls
-use crate::fs::{open_file, OpenFlags, Stat};
-use crate::mm::{translated_byte_buffer, translated_str, UserBuffer};
+use crate::fs::{decrease_nlink, increase_nlink, open_file, File, OpenFlags, Stat, StatMode, ROOT_INODE};
+use crate::mm::{translate_va_to_pa, translated_byte_buffer, translated_str, UserBuffer};
 use crate::task::{current_task, current_user_token};
 
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
@@ -81,7 +81,27 @@ pub fn sys_fstat(_fd: usize, _st: *mut Stat) -> isize {
         "kernel:pid[{}] sys_fstat NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+    //-1
+    let task = current_task().unwrap();
+    let token = current_user_token();
+    let inner = task.inner_exclusive_access();
+    if let Some(file) = &inner.fd_table[_fd] {
+        let st = translate_va_to_pa(token, _st as usize) as *mut Stat;
+        let ino = file.get_inode_id() as u64;
+        let nlink = file.get_nlink() as u32;
+        unsafe {
+            *st = Stat {
+                dev: 0,
+                ino,
+                mode: StatMode::FILE,
+                nlink,
+                pad: [0; 7],
+            };
+        }
+        0
+    } else {
+        -1
+    }
 }
 
 /// YOUR JOB: Implement linkat.
@@ -90,7 +110,20 @@ pub fn sys_linkat(_old_name: *const u8, _new_name: *const u8) -> isize {
         "kernel:pid[{}] sys_linkat NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+    //-1
+    let token = current_user_token();
+    let old_name = translated_str(token, _old_name);
+    let new_name = translated_str(token, _new_name);
+    if old_name == new_name {
+        return -1;
+    }
+    if let Some(old_inode) = open_file(old_name.as_str(), OpenFlags::RDONLY) {
+        ROOT_INODE.link(old_inode.get_inode_id(), new_name.as_str());
+        increase_nlink(old_inode.get_inode_id());
+        0
+    } else {
+        -1
+    }
 }
 
 /// YOUR JOB: Implement unlinkat.
@@ -99,5 +132,17 @@ pub fn sys_unlinkat(_name: *const u8) -> isize {
         "kernel:pid[{}] sys_unlinkat NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+    //-1
+    let token = current_user_token();
+    let name = translated_str(token, _name);
+    if !ROOT_INODE.ls().contains(&name) {
+        return -1;
+    }
+    if let Some(inode) = open_file(name.as_str(), OpenFlags::RDONLY) {
+        ROOT_INODE.unlink(&name);
+        decrease_nlink(inode.get_inode_id());
+        0
+    } else {
+        -1
+    }
 }
